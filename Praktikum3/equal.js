7cd614dcc3d0b72136cc0b69767cbb0f5bd01b1b
Asha Schwegler <schweglerasha@gmail.com>
2022-10-19 13:34:51 +0200

    web 2 und 3

--- Praktikum3/equal.js
+++ Praktikum3/equal.js
@@ -1,18 +1,32 @@
-const { type } = require("os");
-const arrayContent = function(a,b){
+let emptyObj = {}
+function arrayContent(a, b) {
     let a_length = a.length
     let b_length = b.length
-    if(a_length === b_length){
-        for(i = 0; i<=a_length-1;i++){
-            if(a[i].includes(b[i])){
+    if (a_length === b_length) {
+        for (i = 0; i <= a_length - 1; i++) {
+            if (a[i].includes(b[i])) {
                 return true
             }
-            else{
+            else {
                 return false
             }
 
         }
     }
+}
+
+const ObjectContent = function(a,b){
+    let aKeys = Object.keys(a)
+    let bKeys = Object.keys(b)
+    
+    if(aKeys.length!=bKeys.length){
+        return false
+    }
+    for(let key of aKeys){
+        if(a[key] === b[key]){
+            return true;
+        }
+    }return false
 }
 const equal = function(a,b){
     if(a===b){
@@ -21,11 +35,14 @@ const equal = function(a,b){
     else{
         switch(typeof(a)===typeof(b)){
             case true:
-                if(Object.keys(a)===Object.keys(b)){
+                if(ObjectContent(a,b)){
                     return true
                 }
                 else if(arrayContent(a,b)){
                     return true;
+                } else if (JSON.stringify(a) === '{}' && JSON.stringify(b) === '{}') 
+                {
+                    return true
                 }
 
                 break;
@@ -35,4 +52,4 @@ const equal = function(a,b){
         
     } return false;
 }
-module.exports = {equal}
+module.exports={equal}
--- Praktikum3/find-tag.js
+++ Praktikum3/find-tag.js
@@ -1,11 +1,20 @@
 const findTag = function(a){
-    if(a.includes("<"&&">")){
-            let index_begin = a.indexOf("<")
-            let index_end = a.indexOf(">")
-            return a.slice(index_begin+1,index_end)
+    const regex = /<\/?[\w\d]+>/gi
+    let tag = regex.exec(a)
+    if(tag != null){
+        let tag_s = JSON.stringify(tag)
+        let index_begin = tag_s.indexOf("<")        
+        let index_end = tag_s.indexOf(">")
+        return tag_s.slice(index_begin+1,index_end)
 
-        }
+    }
+    
 }
 
+console.log("1",findTag("<header>Text</header"))
+console.log("2",findTag("123245 </header> bla"))
+console.log("3",findTag("123245 <hea der> bla"))
+console.log("4",findTag("123245 <hea<der> bla"))
+console.log("5",findTag("123245 <hea<der bla"))
 
 module.exports = {findTag}
